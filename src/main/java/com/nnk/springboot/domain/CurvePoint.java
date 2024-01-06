3b57f7366817b519585643d01a448268d830bf31
Jamaa <saruul.jamaa@gmail.com>
2024-01-06 19:48:26 +0100

    ajoute des props dans les entities

--- src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -10,5 +10,59 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+    private Integer id;
+    private Integer curveId; Timestamp asOfDate; Double term;
+    private Double value;
+    private Timestamp creationDate;
+
+    public CurvePoint(int curveId, double terme, double value) {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCurveId() {
+        return curveId;
+    }
+
+    public void setCurveId(Integer curveId) {
+        this.curveId = curveId;
+    }
+
+    public Timestamp getAsOfDate() {
+        return asOfDate;
+    }
+
+    public void setAsOfDate(Timestamp asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public Double getTerm() {
+        return term;
+    }
+
+    public void setTerm(Double term) {
+        this.term = term;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
 }
